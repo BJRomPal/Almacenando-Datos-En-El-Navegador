287e4b023a54f96fd9bb216a41cbb054630a361f
Mariano Miró <marianomiro77@gmail.com>
2022-04-28 09:59:59 -0300

    Agregando funcionalidades

---
+++ components/addTask.js
@@ -0,0 +1,54 @@
+import checkComplete from './checkComplete.js';
+import deleteIcon from './deleteIcon.js';
+
+
+const addTask = (evento) => {
+    evento.preventDefault();
+    const list = document.querySelector('[data-list]');
+    const input = document.querySelector('[data-form-input]');
+    const calendar = document.querySelector('[data-form-date]');
+    
+    const value = input.value;
+    const date = calendar.value;
+    const dateFormat = moment(date).format("DD/MM/YYYY");
+    
+    input.value = '';
+    calendar.value = '';
+
+    const taskObj = {
+        value,
+        dateFormat
+      };
+
+    const tasklist = JSON.parse(localStorage.getItem("tasks")) || [];
+    tasklist.push({value, dateFormat})
+    //objeto para almacenar datos en el navegador y que queden persistentes. 
+    localStorage.setItem("tasks", JSON.stringify(tasklist));
+
+    const task = createTask(taskObj)
+    list.appendChild(task);
+  }
+  
+export const createTask = ({value, dateFormat}) => {
+    const task = document.createElement('li');
+            task.classList.add('card');
+    
+    const taskContent = document.createElement('div');
+    
+    const titleTask = document.createElement('span');
+            titleTask.classList.add('task');
+            titleTask.innerText = value;
+            taskContent.appendChild(checkComplete());
+            taskContent.appendChild(titleTask);
+    
+    const dateElement = document.createElement("span")
+            dateElement.innerHTML = dateFormat;
+            task.appendChild(taskContent);
+            task.appendChild(dateElement);
+            task.appendChild(deleteIcon());
+    return task;
+    
+  };
+
+export default addTask;
+  
