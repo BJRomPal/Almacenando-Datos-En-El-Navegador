dc32f7787c2e2b04619a71ef3923aafb63f3958b
Mariano Miró <marianomiro77@gmail.com>
2022-05-01 19:04:07 -0300

    Mejorando el proyecto y agregando la posibilidad de eliminar la tarea

--- components/addTask.js
+++ components/addTask.js
@@ -47,7 +47,6 @@ export const createTask = ({ value, dateFormat, complete, id }) => {
   const check = checkComplete(id);
 
   if(complete) {
-    console.log('completada');
     check.classList.toggle('fas');
     check.classList.toggle('completeIcon');
     check.classList.toggle('far');
@@ -64,6 +63,6 @@ export const createTask = ({ value, dateFormat, complete, id }) => {
 
   task.appendChild(taskContent);
   task.appendChild(dateElement);
-  task.appendChild(deleteIcon());
+  task.appendChild(deleteIcon(id));
   return task;
 };
